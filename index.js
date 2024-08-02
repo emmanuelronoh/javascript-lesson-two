1d33d647ccbd81142fe820394ba278f258e00931
emmanuel ronoh <eronoh036@gmail.com>
2024-08-02 15:28:15 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// let use
+// let password = 12345;rName = "shalyne";
+
+
+// if(userName !== "shalyne" && password !== 12345) {
+//     console.log("succsessfully logged in");
+// }else{
+//     console.log("wrong credentials try again!!");
+   
+// }
+
+function checkCredential(userName, password) {
+    return `Hello  ${userName}   your password is  ${password}`;
+}
+
+console.log(checkCredential("sammy", 12345));
+console.log(checkCredential("sharon", 12345));
+// let userName = "osir";
+// if(userName === "osir") {
+//     let userName = "osir";
+//     console.log("successfully logged in");
+// } 
+// console.log(username)
+
+// function displayName() {
+//     let myName = "caleb";
+//     console.log(myName);
+// }
+// displayName();
